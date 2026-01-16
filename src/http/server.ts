7261aa48a5236d9d7a046ce306ec78ea70e03287
Breno-Mateus <brenomateus36@gmail.com>
2026-01-16 17:24:24 -0300

    add cors

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import { router } from '../routes'; 
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use(router);
 
 app.listen(port, () => {
